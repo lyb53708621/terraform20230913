43d5078f11269425fd2305b298b493c2df75fe61
Edwin Lu <yibolu@microsoft.com>
2023-09-15 15:13:37 +0800

    Use ENV and FMT

--- main.tf
+++ main.tf
@@ -298,14 +298,14 @@ resource "azurerm_virtual_network_peering" "HubToSpoke" {
 #
 #############################################################
 # Create Public IP for Firewall
-/* resource "azurerm_public_ip" "firewallpublicip" {
-  name                      = "firewallpublicip"
-  location                  = azurerm_resource_group.tfrg.location
-  resource_group_name       = azurerm_resource_group.tfrg.name
-  allocation_method         = "Static"
-  sku                       = "Standard"
+resource "azurerm_public_ip" "firewallpublicip" {
+  name                = "firewallpublicip"
+  location            = azurerm_resource_group.tfrg.location
+  resource_group_name = azurerm_resource_group.tfrg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
-
+/*
 # Create Firewall Policy
 resource "azurerm_firewall_policy" "tffwpolicy" {
   name                = "tffwpolicy"
